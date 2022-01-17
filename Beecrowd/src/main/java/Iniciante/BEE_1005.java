4038f9a486417a932dce1b93abe52a5c7900de8f
Feliope <pfelipe@id.uff.br>
2022-01-17 16:52:00 -0300

    Update BEE_1005.java

--- Beecrowd/src/main/java/Iniciante/BEE_1005.java
+++ Beecrowd/src/main/java/Iniciante/BEE_1005.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Iniciante;
 
-/**
- *
- * @author paulo
- */
+import java.util.Scanner;
+
 public class BEE_1005 {
+    public static double A;
+    public static double B;
+    public static double Media;
     
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        A = in.nextDouble();
+        B = in.nextDouble();
+        
+        Media = (((A * 3.5) + (B * 7.5)) / 11);
+        System.out.printf("MEDIA = %.5f\n", Media);
+    }
 }
