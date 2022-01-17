1db7007f8d291e2aa3043c98674ddd6bf0a21c6a
Feliope <pfelipe@id.uff.br>
2022-01-17 16:51:42 -0300

    Update BEE_1003.java

--- Beecrowd/src/main/java/Iniciante/BEE_1003.java
+++ Beecrowd/src/main/java/Iniciante/BEE_1003.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Iniciante;
 
-/**
- *
- * @author paulo
- */
+import java.util.Scanner;
+
 public class BEE_1003 {
+    public static int A;
+    public static int B;
+    public static int SOMA;
     
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        A = in.nextInt();
+        B = in.nextInt();
+        SOMA = A + B;
+        System.out.printf("SOMA = %d\n", SOMA);
+    }
 }
