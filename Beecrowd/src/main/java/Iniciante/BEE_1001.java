763b3b087abea36b7a8d5258d4029dfefb540593
Feliope <pfelipe@id.uff.br>
2022-01-17 16:06:17 -0300

    Criei o BEE_1000

--- Beecrowd/src/main/java/Iniciante/BEE_1001.java
+++ Beecrowd/src/main/java/Iniciante/BEE_1001.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Iniciante;
 
-/**
- *
- * @author paulo
- */
+import java.util.Scanner;
+
 public class BEE_1001 {
     
+    public static int A;
+    public static int B;
+    public static int X;
+    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        A = in.nextInt();
+        B = in.nextInt();
+        X = A + B;
+        System.out.println("X = "+X);
+    }
 }
