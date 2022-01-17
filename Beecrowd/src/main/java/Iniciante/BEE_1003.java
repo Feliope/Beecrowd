d4649025e97ebfdfc711f6d78f1df67c5ce11ece
Paulo Felipe <pfelipe@id.uff.br>
2022-01-17 16:53:28 -0300

    Update BEE_1003.java

--- Beecrowd/src/main/java/Iniciante/BEE_1003.java
+++ Beecrowd/src/main/java/Iniciante/BEE_1003.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BEE_1003 {
     public static int A;
     public static int B;
-    public static int SOMA;
+    public static int SOM;
     
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
